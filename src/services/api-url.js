dda7f0744c1429fc7e099750a05a0d45e7ca375d
Smaraputra <smara.putra2001@gmail.com>
2023-03-01 23:54:21 +0800

    Feat: dynamic link api

--- src/services/api-url.js
+++ src/services/api-url.js
@@ -1,5 +1,9 @@
 export default function apiURL() {
   // console.log('http://'+location.host+'/api/')
   // return 'http://'+location.host+'/api/'
-  return 'https://sipenguideapi.artanode.com/api/'
+  if(location.host=='localhost'){
+    return 'http://'+location.host+'/api/'
+  }else if(location.host=='https://sipenguide.artanode.com/'){
+    return 'https://sipenguideapiapi.artanode.com/api/'
+  }
 }

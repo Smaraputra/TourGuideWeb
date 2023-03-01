78dbe775010e079534068e6e94e74f901b731d11
Smaraputra <smara.putra2001@gmail.com>
2023-03-01 23:50:23 +0800

    Feat: website url update

--- src/services/api-url.js
+++ src/services/api-url.js
@@ -1,4 +1,5 @@
 export default function apiURL() {
-  console.log('http://'+location.host+'/api/')
-  return 'http://'+location.host+'/api/'
+  // console.log('http://'+location.host+'/api/')
+  // return 'http://'+location.host+'/api/'
+  return 'https://sipenguideapi.artanode.com/api/'
 }
